c73e9639214430113d6a97600f53c0cefc5d24f4
faridsaud <faridsaud@yahoo.com>
2016-01-30 00:18:22 -0500

    Dependencias

--- FindClub/app/src/main/java/ec/edu/epn/findclub/CuentaIniciarSesion.java
+++ FindClub/app/src/main/java/ec/edu/epn/findclub/CuentaIniciarSesion.java
@@ -1,7 +1,16 @@
 package ec.edu.epn.findclub;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import ec.edu.epn.findclub.VO.Usuario;
 
 public class CuentaIniciarSesion extends AppCompatActivity {
 
@@ -10,4 +19,30 @@ public class CuentaIniciarSesion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta_iniciar_sesion);
     }
+    private class InvocaServicioREST extends AsyncTask<Void, Void, Usuario> {
+
+        @Override
+        protected Usuario doInBackground(Void... params) {
+            try {
+                EditText tUsuario = (EditText) findViewById(R.id.editText);
+
+                EditText tPassword = (EditText) findViewById(R.id.editText2);
+
+                final String url = "http://192.168.2.149:8080/FindClub/REST/Usuario/buscar?email="+tUsuario.getText()+"&password="+tPassword.getText();
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Usuario usr = restTemplate.getForObject(url, Usuario.class);
+                return usr;
+            } catch (Exception e) {
+                Log.e("Cliente Rest", e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Usuario usr) {
+            Log.d("Usuario", "onPostExecute: "+usr.getEmail());
+        }
+    }
+
 }
