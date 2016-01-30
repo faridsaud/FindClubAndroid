a94436d1c7c7a4f288ecf1570be34b91e9ac980e
faridsaud <faridsaud@yahoo.com>
2016-01-29 23:51:52 -0500

    Navegacion Cuenta
    Moverse entre las distintas actividades

--- FindClub/app/src/main/java/ec/edu/epn/findclub/Cuenta.java
+++ FindClub/app/src/main/java/ec/edu/epn/findclub/Cuenta.java
@@ -1,7 +1,9 @@
 package ec.edu.epn.findclub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Cuenta extends AppCompatActivity {
 
@@ -10,4 +12,26 @@ public class Cuenta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta);
     }
+    public void iniciarSesion(View view){
+
+        Intent intent = new Intent(this, CuentaIniciarSesion.class);
+        startActivity(intent);
+    }
+
+    public void cerrarSesion(View view){
+        Intent intent = new Intent(this, Cuenta.class);
+        startActivity(intent);
+    }
+
+    public void registrar(View view){
+        Intent intent = new Intent(this, CuentaRegistrar.class);
+        startActivity(intent);
+    }
+
+    public void administrar(View view){
+
+        Intent intent = new Intent(this, CuentaAdministrar.class);
+        startActivity(intent);
+    }
+
 }
